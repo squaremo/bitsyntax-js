b8d944e026ef7da94d232c9ad6bfa1802b38a211
Michael Bridgen <mikeb@squaremobius.net>
2012-01-13 12:49:36 +0000

    Tests for binary matches, and fix a bug they revealed

--- test/matching.js
+++ test/matching.js
@@ -1,5 +1,6 @@
 var match = require('../lib/interp').match;
 var v = require('../lib/pattern').variable;
+var rest = require('../lib/pattern').rest;
 var assert = require('assert');
 
 function good(desc, pattern, bytes, bound) {
@@ -8,20 +9,24 @@ function good(desc, pattern, bytes, bound) {
   });
 }
 
+function pattern_str(type, size, flags) {
+  var p = "n:" + size + '/' + type;
+  if (flags.length > 0) {
+    p += '-'+(flags.join('-'));
+  }
+  return p;
+}
+
+function quote(str) {
+  return ["'", "'"].join(str);
+}
+
 suite("Integer",
       function() {
 
-        function pattern_str(size, flags) {
-          var p = "'n:" + size;
-          if (flags.length > 0) {
-              p += "/"+(flags.join('-'));
-          }
-          return p + "'";
-        }
-
         function matches(size, flags, testcases) {
           testcases.forEach(function(testcase) {
-            return good(pattern_str(size, flags),
+            return good(quote(pattern_str('integer', size, flags)),
                         [v('n', size, flags)],
                         testcase[0], {'n': testcase[1]});
           });
@@ -69,4 +74,33 @@ suite("Integer",
           [ [245, 23, 97, 129], -2124343307 ]
         ]);
 
+      });
+
+suite("Binary",
+      function() {
+
+        function matches(testcases) {
+          testcases.forEach(function(testcase) {
+            var size = testcase[0];
+            var flags = ['unit:'+testcase[1]];
+            var desc = pattern_str('binary', size, flags);
+            var descrest = [desc, "_/binary"].join(", ");
+            var plusgarbage = testcase[2].concat([6, 7, 9, 255]);
+            flags.push('binary');
+            good(quote(desc),
+                 [v('n', size, flags)],
+                 testcase[2], {'n': new Buffer(testcase[2])});
+            good(quote(descrest),
+                 [v('n', size, flags), rest()],
+                 plusgarbage, {'n': new Buffer(testcase[2])});
+                });
+        }
+
+        matches([
+          // size, unit, bytes ( expected -> bytes back in a buffer )
+          [ 0, 8, [] ],
+          [ 1, 8, [93] ],
+          [ 5, 8, [1, 2, 3, 4, 5] ],
+          [ 32, 1, [255, 254, 253, 252] ]
+        ]);
       });
