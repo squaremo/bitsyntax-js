fcf8455e87865574bceb9e3a9afd5aff896593da
Michael Bridgen <mikeb@squaremobius.net>
2012-01-13 11:51:04 +0000

    Use sets for flags (specifiers). Make the test cases clearer.

--- test/matching.js
+++ test/matching.js
@@ -8,11 +8,65 @@ function good(desc, pattern, bytes, bound) {
   });
 }
 
-suite("Integers",
+suite("Integer",
       function() {
-        good("uint8", [v('n', 8, [])], [255], {'n': 255});
-        good("int8", [v('n', 8, ['signed'])], [255], {'n': -1});
 
-        good("uint16BE", [v('n', 16, [])], [1, 255], {'n': 511});
-        good("int16BE", [v('n', 16, ['signed'])], [255, 65], {'n': -191});
+        function pattern_str(size, flags) {
+          var p = "'n:" + size;
+          if (flags.length > 0) {
+              p += "/"+(flags.join('-'));
+          }
+          return p + "'";
+        }
+
+        function matches(size, flags, testcases) {
+          testcases.forEach(function(testcase) {
+            return good(pattern_str(size, flags),
+                        [v('n', size, flags)],
+                        testcase[0], {'n': testcase[1]});
+          });
+        }
+
+        matches(8, [], [
+          [ [255], 255 ]
+        ]);
+        matches(8, ['signed'], [
+          [ [255], -1 ]
+        ]);
+        matches(1, ['unit:8'], [
+          [ [129], 129 ],
+        ]);
+        matches(1, ['unit:8', 'signed'], [
+          [ [129], -127 ]
+        ]);
+
+        matches(16, [], [
+          [ [1, 255], 511 ]
+        ]);
+        matches(16, ['signed'], [
+          [ [255, 65], -191 ]
+        ]);
+        matches(16, ['little'], [
+          [ [255, 1], 511 ]
+        ]);
+        matches(16, ['signed', 'little'], [
+          [ [65, 255], -191 ]
+        ]);
+
+        matches(32, [], [
+          [ [45, 23, 97, 102], 756506982 ]
+        ]);
+        matches(32, ['signed'], [
+          [ [245, 23, 97, 102], -183017114 ]
+        ]);
+        matches(32, ['little'], [
+          [ [245, 23, 97, 102], 1717639157 ]
+        ]);
+        matches(32, ['signed', 'little'], [
+          [ [245, 23, 97, 129], -2124343307 ]
+        ]);
+        matches(4, ['unit:8', 'signed', 'little'], [
+          [ [245, 23, 97, 129], -2124343307 ]
+        ]);
+
       });
