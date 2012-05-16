3a8b06efcedf4eb0d6c2e90ed2b1f41b33ab82d7
Michael Bridgen <mikeb@squaremobius.net>
2012-05-16 17:51:58 +0100

    Start at using patterns as constructors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.parse = require('./lib/parse').parse;
 module.exports.match = require('./lib/interp').match;
+module.exports.construct = require('./lib/constructor').construct;
 module.exports.compile = require('./lib/compile').compile;
