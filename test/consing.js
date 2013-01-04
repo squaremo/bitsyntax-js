35687e63ea40c255c666d4680644546b4cb337be
Michael Bridgen <mikeb@squaremobius.net>
2013-01-04 15:10:12 +0000

    Add ability to write into a buffer rather than cons one

---
+++ test/consing.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var parse = require('../lib/parse').parse;
+var construct = require('../lib/constructor').construct;
+var write = require('../lib/constructor').write;
+
+TEST_CASES = [
+  ['n:8', {n:255}, [255]],
+  ['n:16', {n: 0xf0f0}, [240, 240]],
+  ['n:32', {n: 0x12345678}, [18,52,86,120]],
+  ['n:64', {n: 0xffffffffffffffff}, [255,255,255,255,255,255,255,255]],
+
+  ['n:8, s/binary', {n: 255, s: new Buffer("foobar")}, [255, 0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72]],
+  ['n:8, "foobar", m:8', {n: 255, m:0}, [255, 0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72, 0]],
+];
+
+function bufferToArray(buf) {
+  return Array.prototype.slice.call(buf);
+}
+
+suite("Construction", function() {
+  TEST_CASES.forEach(function(c) {
+    var p = parse(c[0]);
+    test(c[0], function() {
+      assert.deepEqual(c[2], bufferToArray(construct(p, c[1])));
+    });
+    test(c[0], function() {
+      var buf = new Buffer(1024);
+      var end = write(buf, 7, p, c[1]);
+      buf = buf.slice(7, end);
+      assert.deepEqual(c[2], bufferToArray(buf));
+    });
+  });
+
+});
