4ccca7a6ef5b6022e401916e82a4542f33379b86
Michael Bridgen <mikeb@squaremobius.net>
2013-10-08 18:53:58 +0100

    Compile constructors too. I mean builders.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ module.exports.build = require('./lib/constructor').build;
 module.exports.write = require('./lib/constructor').write;
 
 module.exports.matcher = require('./lib/compile').compile;
-module.exports.builder = require('./lib/constructor').builder;
+module.exports.builder = require('./lib/compile').compile_builder;
