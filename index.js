a55b2b864ee3b2cfe217d2e5d01028f2d8c7af1a
Michael Bridgen <mikeb@squaremobius.net>
2013-10-08 23:37:37 +0100

    Keep #compile as a synonym for #matcher

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports.match = require('./lib/interp').match;
 module.exports.build = require('./lib/constructor').build;
 module.exports.write = require('./lib/constructor').write;
 
-module.exports.matcher = require('./lib/compile').compile;
+module.exports.matcher = module.exports.compile =
+  require('./lib/compile').compile;
 module.exports.builder = require('./lib/compile').compile_builder;
