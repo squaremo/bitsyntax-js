14b10989c47a7eae0507700b9e0249b8104b7500
Michael Bridgen <mikeb@squaremobius.net>
2013-10-08 23:37:01 +0100

    Account for literal numbers in compiled constructors

--- test/consing.js
+++ test/consing.js
@@ -14,7 +14,8 @@ TEST_CASES = [
   ['n:8, "foobar", m:8', {n: 255, m:0}, [255, 0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72, 0]],
   ['n:8, s:n/binary', {n:6, s: new Buffer('foobar')}, [6, 0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72]],
   ['n:8, s:n/binary', {n:4, s: new Buffer('foobar')}, [4, 0x66, 0x6f, 0x6f, 0x62]],
-  ['n:size', {n:4, size:8}, [4]]
+  ['n:size', {n:4, size:8}, [4]],
+  ['206:n/unit:8', {n:1}, [206]]
 ];
 
 function bufferToArray(buf) {
